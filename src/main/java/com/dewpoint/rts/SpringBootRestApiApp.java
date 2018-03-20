77b528a2c26ccac89cf221983cfee2beffe2212b
KartikShankarappa <kartik.shankarappa@gmail.com>
2018-03-19 20:41:40 -0400

    configured the application to be able to deploy on an external tomcat server

--- src/main/java/com/dewpoint/rts/SpringBootRestApiApp.java
+++ src/main/java/com/dewpoint/rts/SpringBootRestApiApp.java
@@ -2,9 +2,16 @@ package com.dewpoint.rts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"com.dewpoint.rts"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
-public class SpringBootRestApiApp {
+public class SpringBootRestApiApp extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootRestApiApp.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApp.class, args);
